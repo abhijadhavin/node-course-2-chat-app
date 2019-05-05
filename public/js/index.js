0912e3878590594086816041f871ee26ab2b50f0
Abhijeet Jadhav <ethosystem@gmail.com>
2019-05-05 09:10:14 +0530

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -10,25 +10,42 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function(message) {
 	console.log('New Message', message);
+	var li = jQuery('<li></li>');		
+	li.text(`${message.from}: ${message.text}`);
+	jQuery('#messages').append(li);
 });
 
-/*
-socket.emit('createMessage', {
-	from: 'Frank',
-	text: 'Hi'
-}, function (data) {
-	console.log(data);
+socket.on('newLocationMessage', function(message) {
+	var li = jQuery('<li></li>');		
+	var a = jQuery('<a target="_blank"> My Current Location</a>')
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+	jQuery('#messages').append(li);
 });
-*/
+
 jQuery("#message-from").on('submit', function(e){
 	e.preventDefault();	
 	socket.emit('createMessage', {
 	from: 'User',
 	text: jQuery('[name=message]').val()
-	}, function (message) {
-		console.log('new Message', message);
-		var li = jQuery('<li></li>');		
-		li.text(`${message.from}: ${message.text}`);
-		jQuery('#messages').append(li);
+	}, function (message) {		 
 	});
 });
+
+var locationButton = jQuery("#send-location");
+locationButton.on('click', function(e) {
+	e.preventDefault();
+	if (!navigator.geolocation) {
+	  return alert('geolocation not supported by your browser');
+	} 
+	navigator.geolocation.getCurrentPosition(function (postion) {		
+		socket.emit('createLocationMessage', {
+			latitude: postion.coords.latitude,
+			longitude: postion.coords.longitude
+		})
+		console.log(postion);
+	}, function(){
+		alert('unable to fetch location')
+	})
+})
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate correct message Object', () => {
@@ -11,4 +11,18 @@ describe('generateMessage', () => {
 		//expect(message).toHaveProperty({from, text});
 		expect(message).toEqual(expect.objectContaining({from, text}));
 	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location Object', () => {
+		var from = 'jen';
+		var latitude =  15;
+		var longtitude = 19;
+		var url = 'https://www.google.com/maps?q=15,19';
+		var message = generateLocationMessage(from, latitude, longtitude);
+
+		expect(message.createdAt).toBeGreaterThan(0);
+		//expect(message).toHaveProperty({from, text});
+		expect(message).toEqual(expect.objectContaining({from, url}));
+	})	
 });
